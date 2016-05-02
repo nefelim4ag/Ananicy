8e5cd370197d2ac67c34c56c9a6087f88cdcb567
Timofey Titovets <nefelim4ag@gmail.com>
2016-05-03 01:11:22 +0300

    Simplify code of ananicy.sh

--- ananicy.sh
+++ ananicy.sh
@@ -10,12 +10,10 @@ WARN(){ echo -n "WARN: "; echo "$@" ;}
 ERRO(){ echo -n "ERRO: "; echo -n "$@" ; echo " Abort!"; exit 1;}
 
 ################################################################################
-# Initialisation
+# Global vars
 DIR_CACHE=/run/ananicy/
-[ -d "$DIR_CACHE" ] || mkdir -p "$DIR_CACHE"
-
 DIR_CONFIGS=/etc/ananicy.d/
-[ -d "$DIR_CONFIGS" ] || ERRO "Config dir $DIR_CONFIGS doesn't exist!"
+RUN_FREQ=15
 
 ################################################################################
 # Return specified line of file, ignore comments
@@ -47,12 +45,20 @@ done
 
 [ "0" != "${#RULE_CACHE[@]}" ] || ERRO "No rule is enabled!"
 
+show_cache(){
+    for cache_line in "${RULE_CACHE[@]}"; do
+        echo "$cache_line"
+    done
+}
+
+trap "{ INFO Dump compiled rules; show_cache; }" SIGUSR1
 ################################################################################
 # Nice handler for process name
 DIR_CACHE_NICE="$DIR_CACHE/NICE"
 wrapper_renice(){
-    [ -d "$DIR_CACHE_NICE" ] || mkdir -p "$DIR_CACHE_NICE"
     export NAME="$1" NICE="$2"
+    [ -z $NICE ] && return
+    [ -d "$DIR_CACHE_NICE" ] || mkdir -p "$DIR_CACHE_NICE"
     for pid in $( pgrep -w "$NAME" ); do
             LOCK="$DIR_CACHE_NICE/${NAME}.${pid}"
             [ ! -f "$LOCK" ] || OLD_NICE="$(cat $LOCK)"
@@ -67,8 +73,9 @@ wrapper_renice(){
 # IONice handler for process name
 DIR_CACHE_IONICE="$DIR_CACHE/IONICE"
 wrapper_ionice(){
-    [ -d "$DIR_CACHE_IONICE" ] || mkdir -p "$DIR_CACHE_IONICE"
     export NAME="$1" IOCLASS="$2" IONICE="$3"
+    [ "$IOCLASS" == "NULL" ] && [ -z "$IONICE" ] && return
+    [ -d "$DIR_CACHE_IONICE" ] || mkdir -p "$DIR_CACHE_IONICE"
     for pid in $( pgrep -w "$NAME" ); do
             if [ "$IOCLASS" != "NULL" ]; then
                 LOCK="$DIR_CACHE_IONICE/${NAME}.${pid}.ioclass"
@@ -93,42 +100,74 @@ wrapper_ionice(){
     done
 }
 
+check_root_rights(){
+    [ "$UID" == "0" ] || ERRO "Script must be runned as root!"
+}
+
+pre_start_checks(){
+    check_root_rights
+    [ -d "$DIR_CACHE" ] || mkdir -p "$DIR_CACHE"
+    [ -d "$DIR_CONFIGS" ] || ERRO "Config dir $DIR_CONFIGS doesn't exist!"
+}
+
+main_pid_get(){
+    if systemctl -q is-active ananicy; then
+        echo "$(systemctl status ananicy | grep PID | awk '{print $3}')"
+    else
+        ERRO "Ananicy services has stopped!"
+    fi
+}
+
+show_help(){
+    echo "$0 start - start daemon"
+    echo "$0 dump rules cache - daemon will dump rules cache to stdout"
+    echo "$0 dump rules parsed - generate and dump rules cache to stdout"
+}
+
+main_process(){
+    for cache_line in "${RULE_CACHE[@]}"; do
+        NAME="" NICE="" IOCLASS="NULL" IONICE=""
+        for COLUMN in $cache_line; do
+            case "$COLUMN" in
+                NAME=*)    NAME="$(echo $COLUMN | cut -d'=' -f2)"    ;;
+                NICE=*)    NICE="$(echo $COLUMN | cut -d'=' -f2)"    ;;
+                IONICE=*)  IONICE="$(echo $COLUMN | cut -d'=' -f2)"  ;;
+                IOCLASS=*) IOCLASS="$(echo $COLUMN | cut -d'=' -f2)" ;;
+            esac
+        done
+        if [ ! -z "$NAME" ]; then
+            wrapper_renice "$NAME" "$NICE"
+            wrapper_ionice "$NAME" "$IOCLASS" "$IONICE"
+        fi
+    done
+}
+
 ################################################################################
 # Main process
-if [ "$1" == "start" ]; then
-    [ "$UID" == "0" ] || ERRO "Script must be runned as root!"
-    INFO "Start main process"
-    while true; do
-        for cache_line in "${RULE_CACHE[@]}"; do
-            NAME="" NICE="" IOCLASS="" IONICE=""
-            for COLUMN in $cache_line; do
-                if   echo "$COLUMN" | grep -q 'NAME='; then
-                    NAME="$(echo $COLUMN | cut -d'=' -f2)"
-                elif echo "$COLUMN" | grep -q -e '^NICE='; then
-                    NICE="$(echo $COLUMN | cut -d'=' -f2)"
-                elif echo "$COLUMN" | grep -q 'IONICE='; then
-                    IONICE="$(echo $COLUMN | cut -d'=' -f2)"
-                elif echo "$COLUMN" | grep -q 'IOCLASS='; then
-                    IOCLASS="$(echo $COLUMN | cut -d'=' -f2)"
-                fi
-            done
-            if [ ! -z "$NAME" ]; then
-                if [ ! -z "$NICE" ]; then
-                    wrapper_renice "$NAME" "$NICE"
-                fi
-                if [ ! -z "$IOCLASS" ]; then
-                    wrapper_ionice "$NAME" "$IOCLASS" "$IONICE"
-                elif [ ! -z "$IONICE" ]; then
-                    wrapper_ionice "$NAME" "NULL" "$IONICE"
-                fi
-            fi
+case $1 in
+    start)
+        pre_start_checks
+        INFO "Start main process"
+        while true; do
+            main_process
+            sleep $RUN_FREQ
         done
-        sleep 60
-    done
-elif [ "$1" == "dump" ]; then
-    if [ "$2" == "rule_cache" ]; then
-        for cache_line in "${RULE_CACHE[@]}"; do
-            echo "$cache_line"
-        done
-    fi
-fi
+    ;;
+    dump)
+        case "$2" in
+            rules)
+                case "$3" in
+                    cache)
+                        check_root_rights
+                        PID_MAIN="$(main_pid_get)"
+                        kill -s SIGUSR1 $PID_MAIN
+                    ;;
+                    parsed) show_cache ;;
+                    *) show_help ;;
+                esac
+            ;;
+            *) show_help ;;
+        esac
+    ;;
+    *) show_help ;;
+esac
