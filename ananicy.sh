2adbff10e11283e3cbc3838d20d6376f84cf3f30
Timofey Titovets <nefelim4ag@gmail.com>
2016-05-06 17:11:04 +0300

    Random fixes

--- ananicy.sh
+++ ananicy.sh
@@ -10,36 +10,10 @@ WARN(){ echo -n "WARN: "; echo "$@" ;}
 ERRO(){ echo -n "ERRO: "; echo -n "$@" ; echo " Abort!"; exit 1;}
 
 ################################################################################
-# Global vars
-RULE_CACHE=()
-DIR_CACHE=/run/ananicy/
+# Check DIR_CONFIGS
 DIR_CONFIGS=/etc/ananicy.d/
-RUN_FREQ=15
-
-################################################################################
-# Return specified line of file, ignore comments
-read_line_slow(){
-    FILE=$1 NUM=$2 # Read line | remove unsafe symbols | remove comments
-    LINE="$(head -n $NUM $FILE | tail -n 1 | tr -d '$()`' | cut -d'#' -f1)"
-    CHECK=$3
-    # Check supported keys
-    if $CHECK; then
-        case "$LINE" in
-            *NAME=*)
-                case "$LINE" in
-                    *NICE=*)    : ;;
-                    *IOCLASS=*) : ;;
-                    *IONICE=*)  : ;;
-                    *) LINE="" ;;
-                esac
-            ;;
-            *) LINE="";
-        esac
-    fi
-    echo "$LINE"
-}
-
-linecount(){ FILE="$1"; cat "$FILE" | wc -l; }
+INFO "Check $DIR_CONFIGS dir"
+[ -d "$DIR_CONFIGS" ] || ERRO "Config dir $DIR_CONFIGS doesn't exist!"
 
 ################################################################################
 # Load all rule file names
@@ -48,44 +22,63 @@ CONFIGS=( $(find -P $DIR_CONFIGS -type f) )
 [ "0" != "${#CONFIGS[@]}" ] || ERRO "Config dir: $DIR_CONFIGS are empty!"
 
 ################################################################################
+# Return specified line of file, ignore comments
+read_line(){
+    FILE="$1" NUM=$2 # Read line | remove unsafe symbols | remove comments
+    LINE="$(head -n $NUM $FILE | tail -n 1 | tr -d '$()`' | cut -d'#' -f1)"
+    echo "$LINE"
+}
+
+################################################################################
 # Dedup rules
-DIR_TMP=$(mktemp -d)
+declare -A RULE_CACHE_TMP
+
 for config in "${CONFIGS[@]}"; do
-    LINE_COUNT=$(linecount "$config")
+    LINE_COUNT=$(cat "$config" | wc -l)
     for line_number in $(seq 1 $LINE_COUNT); do
-        LINE="$(read_line_slow $config $line_number false)"
+        LINE="$(read_line $config $line_number)"
         if [ ! -z "$LINE" ]; then
             NAME=""
             for COLUMN in $LINE; do
                 case "$COLUMN" in
-                    NAME=*) NAME="$(echo $COLUMN | cut -d'=' -f2)" ;;
+                    NAME=*)
+                        NAME="$(echo $COLUMN | cut -d'=' -f2)"
+                        [ -z "$NAME" ] && ERRO "$config:$line_number NAME are empty!"
+                        ;;
                 esac
             done
-            echo "$LINE" > "$DIR_TMP/$NAME.rule"
+            RULE_CACHE_TMP["$NAME"]="$LINE"
         fi
     done
 done
 
+unset CONFIGS
 ################################################################################
 # Compile rules
 INFO "Compile rule files"
-
-CONFIGS=( $(find -P $DIR_TMP -type f) )
-
-for config in "${CONFIGS[@]}"; do
-    LINE_COUNT=$(linecount "$config")
-    for line_number in $(seq 1 $LINE_COUNT); do
-        LINE="$(read_line_slow $config $line_number true)"
-        [ -z "$LINE" ] || RULE_CACHE=( "${RULE_CACHE[@]}" "$LINE" )
-    done
+RULE_CACHE=()
+for LINE in "${RULE_CACHE_TMP[@]}"; do
+    case "$LINE" in
+        *NAME=*)
+            case "$LINE" in
+                *NICE=*)    : ;;
+                *IOCLASS=*) : ;;
+                *IONICE=*)  : ;;
+                *) LINE="" ;;
+            esac
+        ;;
+        *) LINE="";
+    esac
+    [ -z "$LINE" ] || RULE_CACHE=( "${RULE_CACHE[@]}" "$LINE" )
 done
+unset RULE_CACHE_TMP
 
 [ "0" != "${#RULE_CACHE[@]}" ] || ERRO "No rule is enabled!"
 
 ################################################################################
 # Show cached information
-
 show_cache(){
+    INFO "Dump compiled rules"
     {
         for cache_line in "${RULE_CACHE[@]}"; do
             echo "$cache_line"
@@ -93,32 +86,39 @@ show_cache(){
     } | column -t
 }
 
-trap "{ INFO Dump compiled rules; show_cache; }" SIGUSR1
+trap "{ show_cache; }" SIGUSR1
+################################################################################
+# Cache dir for save nice and ionice state of processes
+DIR_CACHE=/run/ananicy/
 
 ################################################################################
 # Nice handler for process name
-DIR_CACHE_NICE="$DIR_CACHE/NICE"
 wrapper_renice(){
     export NAME="$1" NICE="$2"
     [ -z $NICE ] && return
+
+    DIR_CACHE_NICE="$DIR_CACHE/NICE"
     [ -d "$DIR_CACHE_NICE" ] || mkdir -p "$DIR_CACHE_NICE"
+
     for pid in $( pgrep -w "$NAME" ); do
         LOCK="$DIR_CACHE_NICE/${NAME}.${pid}"
         [ ! -f "$LOCK" ] || OLD_NICE="$(cat $LOCK)"
         if [ "$OLD_NICE" != "$NICE" ]; then
-            echo -en "$NAME\t"
-            renice -n $NICE -p $pid && echo $NICE > $LOCK
+            INFO "Process $NAME cpu nice: $NICE"
+            renice -n $NICE -p $pid &> /dev/null && echo $NICE > $LOCK
         fi
     done
 }
 
 ################################################################################
 # IONice handler for process name
-DIR_CACHE_IONICE="$DIR_CACHE/IONICE"
 wrapper_ionice(){
     export NAME="$1" IOCLASS="$2" IONICE="$3"
     [ "$IOCLASS" == "NULL" ] && [ -z "$IONICE" ] && return
+
+    DIR_CACHE_IONICE="$DIR_CACHE/IONICE"
     [ -d "$DIR_CACHE_IONICE" ] || mkdir -p "$DIR_CACHE_IONICE"
+
     for pid in $( pgrep -w "$NAME" ); do
         LOCK="$DIR_CACHE_IONICE/${NAME}.${pid}.ioclass"
         if [ "$IOCLASS" != "NULL" ]; then
@@ -143,15 +143,7 @@ wrapper_ionice(){
     done
 }
 
-check_root_rights(){
-    [ "$UID" == "0" ] || ERRO "Script must be runned as root!"
-}
-
-pre_start_checks(){
-    check_root_rights
-    [ -d "$DIR_CACHE" ] || mkdir -p "$DIR_CACHE"
-    [ -d "$DIR_CONFIGS" ] || ERRO "Config dir $DIR_CONFIGS doesn't exist!"
-}
+check_root_rights(){ [ "$UID" == "0" ] || ERRO "Script must be runned as root!"; }
 
 main_pid_get(){
     if systemctl -q is-active ananicy; then
@@ -201,9 +193,10 @@ main_process(){
 # Main process
 case $1 in
     start)
-        pre_start_checks
+        check_root_rights
         check_schedulers
         INFO "Start main process"
+        RUN_FREQ=15
         while true; do
             main_process
             sleep $RUN_FREQ
