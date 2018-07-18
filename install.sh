84471bc8172321170dec71a030cb159582e02b5a
Andre Luiz Romano Madureira <alr46664@users.noreply.github.com>
2018-07-18 14:38:13 -0300

    Fixed bug and improved readability

--- install.sh
+++ install.sh
@@ -6,20 +6,14 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 [ "$(whoami)" != "root" ] && echo -e "\n\tRUN this script as ROOT. Exiting...\n" && exit 1
 
-PWD=$(pwd) 
 apt-get update &&
 apt-get -y install schedtool git make gcc autoconf automake python3 &&
-cd "$SCRIPT_DIR"/ananicy &&
 make -j4 install &&
 systemctl daemon-reload &&
 systemctl enable ananicy &&
-systemctl start ananicy 
-STATUS=$?
-cd "$PWD"
-if [ $STATUS -eq 0 ]; then
-  echo -e '\n\tAll went fine, Ananicy Installed with SUCCESS\n'
-else
+systemctl start ananicy &&
+echo -e '\n\tAll went fine, Ananicy Installed with SUCCESS\n' || (
   echo -e '\n\tAnanicy Installation - ERROR\n'
-  exit $STATUS
-fi
+  exit 1
+)
 
