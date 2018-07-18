8d259ddab09382f0144533ffcf08051094191d03
Andre Madureira <andreluizromano@hotmail.com>
2018-07-18 16:21:46 -0300

    improved script for better maintainability

--- install.sh
+++ install.sh
@@ -2,12 +2,26 @@
 # Copywright - Andre L. R. Madureira - 2018
 # Install dependencies to make Ananicy work inside a Linux Distribution
 #
-# Currently supported distros: Debian, ... others go here ...
+# To add a new distro, just add it below the declare -A
+# and type in the command to update and install 
+# dependencies. If you want to specify different 
+# dependencies based on the ARCH of the system, just 
+# type in SUPPORTED_DISTROS[distroname_arch]. Ex:
+# SUPPORTED_DISTROS[debian_x86_64]="apt-get ..."
+declare -A SUPPORTED_DISTROS
 
-SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+SUPPORTED_DISTROS[debian]="
+apt-get update &&
+apt-get install coreutils schedtool make python3"
+
+SUPPORTED_DISTROS[ubuntu]="
+apt-get update &&
+apt-get install coreutils schedtool make python3"
 
 # check if the distro supports systemctl
+SERVICE_NAME=ananicy 
 SYSTEMCTL=$(whereis systemctl | cut -d':' -f2 | tr -s ' ' | xargs | cut -d\  -f1) 
+
 DISTRO=""
 ARCH=""
 
@@ -15,10 +29,13 @@ ARCH=""
 
 check_distro(){
   # find the distro running
-  DISTRO=$( (lsb_release -a ; cat /etc/issue* /etc/*release /proc/version) 2> /dev/null ) 
-  if echo "$DISTRO" | grep -i -P 'debian' &> /dev/null; then
-    DISTRO=debian
-  fi
+  local SUPP_DIST=$(echo ${!SUPPORTED_DISTROS[@]} | tr ' ' '|')
+  DISTRO=$( 
+  (lsb_release -a ; cat /etc/issue* /etc/*release /proc/version) 2> /dev/null |
+  tr '[:upper:]' '[:lower:]' |
+  grep -o -P "($SUPP_DIST)" |
+  head -n 1
+  ) 
   # check distro architecture 
   if uname -a | grep -i -P '(amd64|x86_64)' &> /dev/null; then
     ARCH=x86_64
@@ -27,23 +44,15 @@ check_distro(){
   fi  
 }
 
-install_deps(){    
-  local LOCAL_STATUS=0
-  case "${DISTRO}_${ARCH}" in
-  debian*|ubuntu*)
-    apt-get update &&
-    apt-get -y install coreutils schedtool make python3
-    LOCAL_STATUS=$(($LOCAL_STATUS+$?))
-    ;;
-  *)
-    # add other distros here as needed
-    ;;
-  esac
-  return $LOCAL_STATUS
+install_deps(){      
+  local PKT_TOOLS=${SUPPORTED_DISTROS[${DISTRO}_${ARCH}]}
+  if [ -z "$PKT_TOOLS" ]; then
+    PKT_TOOLS=${SUPPORTED_DISTROS[${DISTRO}]}
+  fi  
+  eval $PKT_TOOLS
 }
 
 set_autostart(){
-  local SERVICE_NAME=ananicy   
   local LOCAL_STATUS=0
   if [ -n "$SYSTEMCTL" ]; then
     # use systemctl to autostart ananicy
@@ -52,7 +61,9 @@ set_autostart(){
     systemctl start "$SERVICE_NAME" 
     LOCAL_STATUS=$(($LOCAL_STATUS+$?))
   else
-    # TODO, create SYS V INIT script
+    # TODO, create SYS V INIT script and remove the 
+    # LOCAL_STATUS=1 below
+    LOCAL_STATUS=1
   fi  
   return $LOCAL_STATUS
 }
